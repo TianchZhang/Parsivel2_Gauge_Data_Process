1159fd39abbe08da982b65ec7e08e14b70cb8924
Tianch Zhang <solar.ztc@gmail.com>
2021-12-13 23:19:40 +0800

    update

--- Parameters_Calculate/DSD_nonQC_allR.m
+++ Parameters_Calculate/DSD_nonQC_allR.m
@@ -9,8 +9,8 @@ clear;
 nonrain = ["20190404";"20190405";"20190406";"20190407";...
     "20191218";"20200109";"20200115";"20200116";"20200125";...
     "20200215";"20201213";"20201214";"20201229"];
-savepath ='D:\DATA\OTTParsivel\nonQC2019-\';
-file_root = 'D:\DATA\OTTParsivel\57494\Mputu\';
+savepath ='E:\DATA\OTTParsivel\nonQC2019-\';
+file_root = 'E:\DATA\OTTParsivel\57494\Mputu\';
 file_day = dir(file_root);
 load('D:\DATA\Parsivel_temporary\DSD_parameters.mat','speed_coe');
 load('D:\DATA\Parsivel_temporary\DSD_parameters.mat','central_diameter');
