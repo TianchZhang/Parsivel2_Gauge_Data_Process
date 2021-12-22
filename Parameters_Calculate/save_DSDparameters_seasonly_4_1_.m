c70f3a8dccf602a6748f69246579457fe029a18f
Tianch Zhang <solar.ztc@gmail.com>
2021-12-22 16:32:24 +0800

    update

--- Parameters_Calculate/save_DSDparameters_seasonly_4_1_.m
+++ Parameters_Calculate/save_DSDparameters_seasonly_4_1_.m
@@ -345,47 +345,47 @@ mu_conv_2020spr_ev = [];
 lamd_stra_2020spr_ev = [];
 lamd_conv_2020spr_ev = [];
 %
-ND_conv_2021sum = [];
-ND_stra_2021sum =[];
-Nw_conv_2021sum = [];
-Nw_stra_2021sum = [];
-N0_stra_2021sum = [];
-N0_conv_2021sum = [];
-Dm_conv_2021sum = [];
-Dm_stra_2021sum = [];
-Delt_m_stra_2021sum = [];
-Delt_m_conv_2021sum = [];
-LWC_stra_2021sum = [];
-LWC_conv_2021sum = [];
-RR_conv_2021sum = [];
-RR_stra_2021sum = [];
-Z_conv_2021sum = [];
-Z_stra_2021sum = [];
-mu_stra_2021sum = [];
-mu_conv_2021sum = [];
-lamd_stra_2021sum = [];
-lamd_conv_2021sum = [];
-%
-ND_conv_2021sum = [];
-ND_stra_2021sum =[];
-Nw_conv_2021sum = [];
-Nw_stra_2021sum = [];
-N0_stra_2021sum = [];
-N0_conv_2021sum = [];
-Dm_conv_2021sum = [];
-Dm_stra_2021sum = [];
-Delt_m_stra_2021sum = [];
-Delt_m_conv_2021sum = [];
-LWC_stra_2021sum = [];
-LWC_conv_2021sum = [];
-RR_conv_2021sum = [];
-RR_stra_2021sum = [];
-Z_conv_2021sum = [];
-Z_stra_2021sum = [];
-mu_stra_2021sum = [];
-mu_conv_2021sum = [];
-lamd_stra_2021sum = [];
-lamd_conv_2021sum_ev = [];
+% ND_conv_2021sum = [];
+% ND_stra_2021sum =[];
+% Nw_conv_2021sum = [];
+% Nw_stra_2021sum = [];
+% N0_stra_2021sum = [];
+% N0_conv_2021sum = [];
+% Dm_conv_2021sum = [];
+% Dm_stra_2021sum = [];
+% Delt_m_stra_2021sum = [];
+% Delt_m_conv_2021sum = [];
+% LWC_stra_2021sum = [];
+% LWC_conv_2021sum = [];
+% RR_conv_2021sum = [];
+% RR_stra_2021sum = [];
+% Z_conv_2021sum = [];
+% Z_stra_2021sum = [];
+% mu_stra_2021sum = [];
+% mu_conv_2021sum = [];
+% lamd_stra_2021sum = [];
+% lamd_conv_2021sum = [];
+% %
+% ND_conv_2021sum = [];
+% ND_stra_2021sum =[];
+% Nw_conv_2021sum = [];
+% Nw_stra_2021sum = [];
+% N0_stra_2021sum = [];
+% N0_conv_2021sum = [];
+% Dm_conv_2021sum = [];
+% Dm_stra_2021sum = [];
+% Delt_m_stra_2021sum = [];
+% Delt_m_conv_2021sum = [];
+% LWC_stra_2021sum = [];
+% LWC_conv_2021sum = [];
+% RR_conv_2021sum = [];
+% RR_stra_2021sum = [];
+% Z_conv_2021sum = [];
+% Z_stra_2021sum = [];
+% mu_stra_2021sum = [];
+% mu_conv_2021sum = [];
+% lamd_stra_2021sum = [];
+% lamd_conv_2021sum_ev = [];
 
 for inum = 1:length(Rainev_day)
     mkey = fix(str2num(Rainev_day{inum,1})/100);
@@ -791,7 +791,12 @@ for inum = 1:length(Rainev_day)
     end
     
 end
-
+clear Delt_m Dm lamd LWC mu N0 ND Nw RR Z
+clear tempDem tempDm templamd tempLWC tempmu tempN0 tempND tempNw tempRR temptype tempZ typeflag
+clear loclen mkey rainloc
+clear Rainev_day Rainev_detal
+%%
+%all
 Nw_conv_all = [Nw_conv_2019spr;Nw_conv_2020spr;Nw_conv_2020sum;Nw_conv_2019sum;...
     Nw_conv_2019aut;Nw_conv_2020aut;Nw_conv_2019win;Nw_conv_2020win];
 Dm_conv_all = [Dm_conv_2019spr;Dm_conv_2020spr;Dm_conv_2020sum;Dm_conv_2019sum;...
@@ -882,4 +887,106 @@ lamd_conv_all_ev = [lamd_conv_2019spr_ev;lamd_conv_2020spr_ev;lamd_conv_2020sum_
 lamd_stra_all_ev = [lamd_stra_2019spr_ev;lamd_stra_2020spr_ev;lamd_stra_2020sum_ev;lamd_stra_2019sum_ev;...
     lamd_stra_2019aut_ev;lamd_stra_2020aut_ev;lamd_stra_2019win_ev;lamd_stra_2020win_ev];
 
+%%
+%2019
+Nw_conv_2019 = [Nw_conv_2019spr;Nw_conv_2019sum;Nw_conv_2019aut;Nw_conv_2019win];
+Dm_conv_2019 = [Dm_conv_2019spr;Dm_conv_2019sum;Dm_conv_2019aut;Dm_conv_2019win];
+Nw_stra_2019 = [Nw_stra_2019spr;Nw_stra_2019sum;Nw_stra_2019aut;Nw_stra_2019win];
+Dm_stra_2019 = [Dm_stra_2019spr;Dm_stra_2019sum;Dm_stra_2019aut;Dm_stra_2019win];
+Nw_conv_2019_ev = [Nw_conv_2019spr_ev;Nw_conv_2019sum_ev;Nw_conv_2019aut_ev;Nw_conv_2019win_ev];
+Dm_conv_2019_ev = [Dm_conv_2019spr_ev;Dm_conv_2019sum_ev;Dm_conv_2019aut_ev;Dm_conv_2019win_ev];
+Nw_stra_2019_ev = [Nw_stra_2019spr_ev;Nw_stra_2019sum_ev;Nw_stra_2019aut_ev;Nw_stra_2019win_ev;];
+Dm_stra_2019_ev = [Dm_stra_2019spr_ev;Dm_stra_2019sum_ev;Dm_stra_2019aut_ev;Dm_stra_2019win_ev];
 
+RR_conv_2019 = [RR_conv_2019spr;RR_conv_2019sum;RR_conv_2019aut;RR_conv_2019win];
+RR_stra_2019 = [RR_stra_2019spr;RR_stra_2019sum;RR_stra_2019aut;RR_stra_2019win];
+RR_conv_2019_ev = [RR_conv_2019spr_ev;RR_conv_2019sum_ev;RR_conv_2019aut_ev;RR_conv_2019win_ev];
+RR_stra_2019_ev = [RR_stra_2019spr_ev;RR_stra_2019sum_ev;RR_stra_2019aut_ev;RR_stra_2019win_ev;];
+
+Z_conv_2019 = [Z_conv_2019spr;Z_conv_2019sum;Z_conv_2019aut;Z_conv_2019win];
+Z_stra_2019 = [Z_stra_2019spr;Z_stra_2019sum; Z_stra_2019aut;Z_stra_2019win];
+Z_conv_2019_ev = [Z_conv_2019spr_ev;Z_conv_2019sum_ev;Z_conv_2019aut_ev;Z_conv_2019win_ev;];
+Z_stra_2019_ev = [Z_stra_2019spr_ev;Z_stra_2019sum_ev;Z_stra_2019aut_ev;Z_stra_2019win_ev];
+
+N0_conv_2019 = [N0_conv_2019spr;N0_conv_2019sum;N0_conv_2019aut;N0_conv_2019win];
+N0_stra_2019 = [N0_stra_2019spr;N0_stra_2019sum;N0_stra_2019aut;N0_stra_2019win];
+N0_conv_2019_ev = [N0_conv_2019spr_ev;N0_conv_2019sum_ev;N0_conv_2019aut_ev;N0_conv_2019win_ev];
+N0_stra_2019_ev = [N0_stra_2019spr_ev;N0_stra_2019sum_ev;N0_stra_2019aut_ev;N0_stra_2019win_ev];
+
+ND_conv_2019 = [ND_conv_2019spr;ND_conv_2019sum;ND_conv_2019aut;ND_conv_2019win];
+ND_stra_2019 = [ND_stra_2019spr;ND_stra_2019sum;ND_stra_2019aut;ND_stra_2019win];
+ND_conv_2019_ev = [ND_conv_2019spr_ev;ND_conv_2019sum_ev;ND_conv_2019aut_ev;ND_conv_2019win_ev];
+ND_stra_2019_ev = [ND_stra_2019spr_ev;ND_stra_2019sum_ev;ND_stra_2019aut_ev;ND_stra_2019win_ev];
+
+Delt_m_conv_2019 = [Delt_m_conv_2019spr;Delt_m_conv_2019sum;Delt_m_conv_2019aut;Delt_m_conv_2019win];
+Delt_m_stra_2019 = [Delt_m_stra_2019spr;Delt_m_stra_2019sum;Delt_m_stra_2019aut;Delt_m_stra_2019win];
+Delt_m_conv_2019_ev = [Delt_m_conv_2019spr_ev;Delt_m_conv_2019sum_ev;Delt_m_conv_2019aut_ev;Delt_m_conv_2019win_ev];
+Delt_m_stra_2019_ev = [Delt_m_stra_2019spr_ev;Delt_m_stra_2019sum_ev;Delt_m_stra_2019aut_ev;Delt_m_stra_2019win_ev];
+
+LWC_conv_2019 = [LWC_conv_2019spr;LWC_conv_2019sum;LWC_conv_2019aut;LWC_conv_2019win];
+LWC_stra_2019 = [LWC_stra_2019spr;LWC_stra_2019sum;LWC_stra_2019aut;LWC_stra_2019win];
+LWC_conv_2019_ev = [LWC_conv_2019spr_ev;LWC_conv_2019sum_ev;LWC_conv_2019aut_ev;LWC_conv_2019win_ev];
+LWC_stra_2019_ev = [LWC_stra_2019spr_ev;LWC_stra_2019sum_ev;LWC_stra_2019aut_ev;LWC_stra_2019win_ev];
+
+mu_conv_2019 = [mu_conv_2019spr;mu_conv_2019sum;mu_conv_2019aut;mu_conv_2019win];
+mu_stra_2019 = [mu_stra_2019spr;mu_stra_2019sum;mu_stra_2019aut;mu_stra_2019win];
+mu_conv_2019_ev = [mu_conv_2019spr_ev;mu_conv_2019sum_ev;mu_conv_2019aut_ev;mu_conv_2019win_ev];
+mu_stra_2019_ev = [mu_stra_2019spr_ev;mu_stra_2019sum_ev;mu_stra_2019aut_ev;mu_stra_2019win_ev];
+
+lamd_conv_2019 = [lamd_conv_2019spr;lamd_conv_2019sum;lamd_conv_2019aut;lamd_conv_2019win];
+lamd_stra_2019 = [lamd_stra_2019spr;lamd_stra_2019sum;lamd_stra_2019aut;lamd_stra_2019win];
+lamd_conv_2019_ev = [lamd_conv_2019spr_ev;lamd_conv_2019sum_ev;lamd_conv_2019aut_ev;lamd_conv_2019win_ev];
+lamd_stra_2019_ev = [lamd_stra_2019spr_ev;lamd_stra_2019sum_ev;lamd_stra_2019aut_ev;lamd_stra_2019win_ev];
+%%
+Nw_conv_2020 = [Nw_conv_2020spr;Nw_conv_2020sum;Nw_conv_2020aut;Nw_conv_2020win];
+Dm_conv_2020 = [Dm_conv_2020spr;Dm_conv_2020sum;Dm_conv_2020aut;Dm_conv_2020win];
+Nw_stra_2020 = [Nw_stra_2020spr;Nw_stra_2020sum;Nw_stra_2020aut;Nw_stra_2020win];
+Dm_stra_2020 = [Dm_stra_2020spr;Dm_stra_2020sum;Dm_stra_2020aut;Dm_stra_2020win];
+Nw_conv_2020_ev = [Nw_conv_2020spr_ev;Nw_conv_2020sum_ev;Nw_conv_2020aut_ev;Nw_conv_2020win_ev];
+Dm_conv_2020_ev = [Dm_conv_2020spr_ev;Dm_conv_2020sum_ev;Dm_conv_2020aut_ev;Dm_conv_2020win_ev];
+Nw_stra_2020_ev = [Nw_stra_2020spr_ev;Nw_stra_2020sum_ev;Nw_stra_2020aut_ev;Nw_stra_2020win_ev];
+Dm_stra_2020_ev = [Dm_stra_2020spr_ev;Dm_stra_2020sum_ev;Dm_stra_2020aut_ev;Dm_stra_2020win_ev];
+
+RR_conv_2020 = [RR_conv_2020spr;RR_conv_2020sum;RR_conv_2020aut;RR_conv_2020win];
+RR_stra_2020 = [RR_stra_2020spr;RR_stra_2020sum;RR_stra_2020aut;RR_stra_2020win];
+RR_conv_2020_ev = [RR_conv_2020spr_ev;RR_conv_2020sum_ev;RR_conv_2020aut_ev;RR_conv_2020win_ev];
+RR_stra_2020_ev = [RR_stra_2020spr_ev;RR_stra_2020sum_ev;RR_stra_2020aut_ev;RR_stra_2020win_ev];
+
+
+Z_conv_2020 = [Z_conv_2020spr;Z_conv_2020sum;Z_conv_2020aut;Z_conv_2020win];
+Z_stra_2020 = [Z_stra_2020spr;Z_stra_2020sum;Z_stra_2020aut;Z_stra_2020win];
+Z_conv_2020_ev = [Z_conv_2020spr_ev;Z_conv_2020sum_ev;Z_conv_2020aut_ev;Z_conv_2020win_ev];
+Z_stra_2020_ev = [Z_stra_2020spr_ev;Z_stra_2020sum_ev;Z_stra_2020aut_ev;Z_stra_2020win_ev];
+
+N0_conv_2020 = [N0_conv_2020spr;N0_conv_2020sum;N0_conv_2020aut;N0_conv_2020win];
+N0_stra_2020 = [N0_stra_2020spr;N0_stra_2020sum;N0_stra_2020aut;N0_stra_2020win];
+N0_conv_2020_ev = [N0_conv_2020spr_ev;N0_conv_2020sum_ev;N0_conv_2020aut_ev;N0_conv_2020win_ev];
+N0_stra_2020_ev = [N0_stra_2020spr_ev;N0_stra_2020sum_ev;N0_stra_2020aut_ev;N0_stra_2020win_ev];
+
+ND_conv_2020 = [ND_conv_2020spr;ND_conv_2020sum;ND_conv_2020aut;ND_conv_2020win];
+ND_stra_2020 = [ND_stra_2020spr;ND_stra_2020sum;ND_stra_2020aut;ND_stra_2020win];
+ND_conv_2020_ev = [ND_conv_2020spr_ev;ND_conv_2020sum_ev;ND_conv_2020aut_ev;ND_conv_2020win_ev];
+ND_stra_2020_ev = [ND_stra_2020spr_ev;ND_stra_2020sum_ev;ND_stra_2020aut_ev;ND_stra_2020win_ev];
+
+Delt_m_conv_2020 = [Delt_m_conv_2020spr;Delt_m_conv_2020sum;Delt_m_conv_2020aut;Delt_m_conv_2020win];
+Delt_m_stra_2020 = [Delt_m_stra_2020spr;Delt_m_stra_2020sum;Delt_m_stra_2020aut;Delt_m_stra_2020win];
+Delt_m_conv_2020_ev = [Delt_m_conv_2020spr_ev;Delt_m_conv_2020sum_ev;Delt_m_conv_2020aut_ev;Delt_m_conv_2020win_ev];
+Delt_m_stra_2020_ev = [Delt_m_stra_2020spr_ev;Delt_m_stra_2020sum_ev;Delt_m_stra_2020aut_ev;Delt_m_stra_2020win_ev];
+
+LWC_conv_2020 = [LWC_conv_2020spr;LWC_conv_2020sum;LWC_conv_2020aut;LWC_conv_2020win];
+LWC_stra_2020 = [LWC_stra_2020spr;LWC_stra_2020sum;LWC_stra_2020aut;LWC_stra_2020win];
+LWC_conv_2020_ev = [LWC_conv_2020spr_ev;LWC_conv_2020sum_ev;LWC_conv_2020aut_ev;LWC_conv_2020win_ev];
+LWC_stra_2020_ev = [LWC_stra_2020spr_ev;LWC_stra_2020sum_ev;LWC_stra_2020aut_ev;LWC_stra_2020win_ev];
+
+mu_conv_2020 = [mu_conv_2020spr;mu_conv_2020sum;mu_conv_2020aut;mu_conv_2020win];
+mu_stra_2020 = [mu_stra_2020spr;mu_stra_2020sum;mu_stra_2020aut;mu_stra_2020win];
+mu_conv_2020_ev = [mu_conv_2020spr_ev;mu_conv_2020sum_ev;mu_conv_2020aut_ev;mu_conv_2020win_ev];
+mu_stra_2020_ev = [mu_stra_2020spr_ev;mu_stra_2020sum_ev;mu_stra_2020aut_ev;mu_stra_2020win_ev];
+
+lamd_conv_2020 = [lamd_conv_2020spr;lamd_conv_2020sum;lamd_conv_2020aut;lamd_conv_2020win];
+lamd_stra_2020 = [lamd_stra_2020spr;lamd_stra_2020sum;lamd_stra_2020aut;lamd_stra_2020win];
+lamd_conv_2020_ev = [lamd_conv_2020spr_ev;lamd_conv_2020sum_ev;lamd_conv_2020aut_ev;lamd_conv_2020win_ev];
+lamd_stra_2020_ev = [lamd_stra_2020spr_ev;lamd_stra_2020sum_ev;lamd_stra_2020aut_ev;lamd_stra_2020win_ev];
+
+save('D:\DATA\Parsivel_temporary\DSD_parameters_difseason.mat');
+save('E:\DATA\Parsivel_temporary\DSD_parameters_difseason.mat');
