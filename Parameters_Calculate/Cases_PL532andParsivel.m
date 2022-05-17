919435586aeee7308854bed2550cb648dd88d5e7
Tianch Zhang <solar.ztc@gmail.com>
2022-05-17 10:12:51 +0800

    update

--- Parameters_Calculate/Cases_PL532andParsivel.m
+++ Parameters_Calculate/Cases_PL532andParsivel.m
@@ -16,14 +16,30 @@ end
 PL_conv_30 = {};
 for ik = 1:length(Conv_day_30)
     if any(strcmp(Conv_day_30{ik,1},pname))
-        PL_conv_30 = [PL_conv_30;Conv_day_30{ik,1}];
+        tempconv = h5read(['D:\DATA\OTTParsivel\nonQC2019-\',Conv_day_30{ik,1},'.h5'],'/typeflag');
+        if length(find(tempconv ==1)) >10
+            PL_conv_30 = [PL_conv_30;Conv_day_30{ik,1}];
+        end
     end
 end
 PL_stra_30 = {};
 for ik = 1:length(Stra_day_30)
     if any(strcmp(Stra_day_30{ik,1},pname))
-        PL_stra_30 = [PL_stra_30;Stra_day_30{ik,1}];
+        tempstra = h5read(['D:\DATA\OTTParsivel\nonQC2019-\',Stra_day_30{ik,1},'.h5'],'/typeflag');
+        if length(find(tempstra ==2)) >10
+            PL_stra_30 = [PL_stra_30;Stra_day_30{ik,1}];
+        end
     end
 end
-save('D:\DATA\Parsivel_temporary\PL_Rainevents.mat','PL_stra_30','PL_conv_30');
+PL_ltstra_30 = {};
+for ik = 1:length(Stra_day_30)
+    if any(strcmp(Stra_day_30{ik,1},pname))
+        tempstra = h5read(['D:\DATA\OTTParsivel\nonQC2019-\',Stra_day_30{ik,1},'.h5'],'/typeflag');
+        tempnw = h5read(['D:\DATA\OTTParsivel\nonQC2019-\',Stra_day_30{ik,1},'.h5'],'/Nw');
+        if length(find(tempstra ==2 & log10(tempnw)>4.4)) >10
+            PL_ltstra_30 = [PL_ltstra_30;Stra_day_30{ik,1}];
+        end
+    end
+end
+save('D:\DATA\Parsivel_temporary\PL_Rainevents.mat','PL_stra_30','PL_ltstra_30','PL_conv_30');
 % save('E:\DATA\Parsivel_temporary\PL_Rainevents.mat','PL_Stra_day_30','PL_Stra_detal_30');
